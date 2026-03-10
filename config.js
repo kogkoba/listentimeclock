4f5b28278f98d13a4029015d1e567597fb1ba987
kogkoba <kc.koba@gmail.com>
2026-03-10 14:44:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-
 const CONFIG = {
   APP_NAME: "LISTEN Time Clock",
-  GAS_URL: ""
+  GAS_URL: "https://script.google.com/macros/s/AKfycbxhTwQCgewRwqKBO6d9Lc7FPBGmQGklAi4jgot_PeYksfNXWYxA6-kTuKGsFZ27LiL7/exec"
 };
